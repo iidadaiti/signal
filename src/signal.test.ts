c9ab00649d55ec6a364b6c2bcf598e3542198de8
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-27 08:39:29 +0000

    add effect

--- src/signal.test.ts
+++ src/signal.test.ts
@@ -1,5 +1,5 @@
-import { describe, it, expect } from "vitest";
-import { createSignal } from "./signal";
+import { describe, it, expect, vi } from "vitest";
+import { createEffect, createSignal } from "./signal";
 
 describe("createSignal", () => {
   it("should initialize the signal with the value passed as an argument", () => {
@@ -23,9 +23,8 @@ describe("createSignal", () => {
   });
 
   it("should update value when setter is called", () => {
-    const [get, set] = createSignal(10);
-
-    const value = 42;
+    const [get, set] = createSignal(42);
+    const value = 43;
     set(value);
     expect(get()).toBe(value);
   });
@@ -37,5 +36,118 @@ describe("createSignal", () => {
 
     value.value = 43;
     expect(get()).not.toEqual(value);
+  });
+});
+
+describe("createEffect", () => {
+  it("should execute createEffect once when created", () => {
+    const handler = vi.fn();
+    createEffect(handler);
+    expect(handler).toBeCalledTimes(1);
+  });
+
+  it("should recalculate when the value is updated", () => {
+    const [get, set] = createSignal(42);
+    const handler = vi.fn(() => {
+      get();
+    });
+
+    createEffect(handler);
+    set(43);
+
+    expect(handler).toBeCalledTimes(2);
+  });
+
+  it("should execute nested effects", () => {
+    const [get, set] = createSignal(42);
+    const handler = vi.fn();
+
+    createEffect(() => {
+      get();
+      createEffect(handler);
+    });
+    set(43);
+
+    expect(handler).toBeCalledTimes(2);
+  });
+
+  it("should not register the same handler multiple times", () => {
+    const [get, set] = createSignal(42);
+    const handler = vi.fn(() => {
+      get();
+      get();
+    });
+
+    createEffect(handler);
+    set(43);
+
+    expect(handler).toBeCalledTimes(2);
+  });
+
+  it("should not refire effects that have been cleaned up", () => {
+    const [get, set] = createSignal(42);
+    const handler = vi.fn(() => {
+      get();
+    });
+
+    const cleanup = createEffect(handler);
+    cleanup();
+    set(43);
+
+    expect(handler).toBeCalledTimes(1);
+  });
+
+  it("should automatically clean up the effects", () => {
+    const [getFlag, setFlag] = createSignal(true);
+    const [getValue, setValue] = createSignal(42);
+
+    const handler = vi.fn(() => {
+      if (getFlag()) {
+        getValue();
+      }
+    });
+    createEffect(handler);
+    setValue(43);
+    setFlag(false);
+    setValue(44);
+
+    expect(handler).toBeCalledTimes(3);
+  });
+
+  it("should unsubscribe from dataNode when it's no longer relevant", () => {
+    const [getFlag, setFlag] = createSignal(true);
+    const [getValue, setValue] = createSignal(42);
+
+    const handler = vi.fn(() => {
+      if (getFlag()) {
+        getValue();
+      }
+    });
+    createEffect(handler);
+    setValue(43);
+    setFlag(false);
+    setValue(44);
+
+    expect(handler).toBeCalledTimes(3);
+  });
+
+  it("should automatically clean up the effects", () => {
+    const [getFlag, setFlag] = createSignal(true);
+    const [getValue, setValue] = createSignal(42);
+
+    const handler = vi.fn(() => {
+      getValue();
+    });
+
+    createEffect(() => {
+      if (getFlag()) {
+        createEffect(handler);
+      }
+    });
+    setValue(43);
+    setFlag(false);
+    setValue(44);
+
+    expect(handler).toBeCalledTimes(2);
   });
 });
--- src/signal.ts
+++ src/signal.ts
@@ -4,17 +4,88 @@ export type Signal<GetValue, SetValue extends GetValue = GetValue> = [
   Getter<GetValue>,
   Setter<SetValue>
 ];
+export type Handler = () => void;
 
 interface DataNode<Value> {
   value: Value;
+  handlers: Set<Handler>;
 }
 
+interface EffectNode {
+  handler: Handler;
+  dataNodes: Set<DataNode<unknown>>;
+  parentEffectNodes: Set<EffectNode>;
+  childEffectNodes: Set<EffectNode>;
+}
+
+let effectNodeContext: EffectNode | undefined;
+
 function createDataNode<Value>(value: Value): DataNode<Value> {
-  return { value: structuredClone(value) };
+  const handlers = new Set<Handler>();
+  return { value: structuredClone(value), handlers };
+}
+
+function createCleanupEffect(effectNode: EffectNode): () => void {
+  return function cleanup() {
+    for (const dataNode of effectNode.dataNodes) {
+      dataNode.handlers.delete(effectNode.handler);
+    }
+
+    for (const childEffectNode of effectNode.childEffectNodes) {
+      createCleanupEffect(childEffectNode)();
+    }
+
+    for (const parentEffectNode of effectNode.parentEffectNodes) {
+      parentEffectNode.childEffectNodes.delete(effectNode);
+    }
+  };
+}
+
+function createEffectNode(handler: Handler): EffectNode {
+  const dataNodes = new Set<DataNode<unknown>>();
+  const childEffectNodes = new Set<EffectNode>();
+  const parentEffectNodes = new Set<EffectNode>();
+  const effectNode: EffectNode = {
+    dataNodes,
+    childEffectNodes,
+    parentEffectNodes,
+    handler: () => {
+      if (effectNodeContext) {
+        effectNodeContext.childEffectNodes.add(effectNode);
+        effectNode.parentEffectNodes.add(effectNodeContext);
+      }
+
+      const beforeEffectNodeContext = effectNodeContext;
+      const beforeGotDataNodes = new Set(effectNode.dataNodes);
+      const beforeEffectNodes = new Set(effectNode.childEffectNodes);
+      effectNode.dataNodes.clear();
+      effectNode.childEffectNodes.clear();
+      effectNodeContext = effectNode;
+      handler();
+      for (const beforeGotDataNode of beforeGotDataNodes) {
+        if (!effectNode.dataNodes.has(beforeGotDataNode)) {
+          beforeGotDataNode.handlers.delete(effectNode.handler);
+        }
+      }
+
+      for (const beforeEffectNode of beforeEffectNodes) {
+        if (!effectNode.childEffectNodes.has(beforeEffectNode)) {
+          createCleanupEffect(beforeEffectNode)();
+        }
+      }
+      effectNodeContext = beforeEffectNodeContext;
+    },
+  };
+
+  return effectNode;
 }
 
 function createGetter<Value>(dataNode: DataNode<Value>): Getter<Value> {
   return function getter() {
+    if (effectNodeContext) {
+      effectNodeContext.dataNodes.add(dataNode);
+      dataNode.handlers.add(effectNodeContext.handler);
+    }
     return structuredClone(dataNode.value);
   };
 }
@@ -24,6 +95,9 @@ function createSetter<GetValue, SetValue extends GetValue = GetValue>(
 ): Setter<SetValue> {
   return function setter(value: SetValue) {
     dataNode.value = structuredClone(value);
+    for (const handler of dataNode.handlers) {
+      handler();
+    }
   };
 }
 
@@ -40,4 +114,11 @@ export function createSignal<
   const getter = createGetter(dataNode);
   const setter = createSetter(dataNode);
   return [getter, setter];
+}
+
+export function createEffect(handler: Handler): () => void {
+  const effectNode = createEffectNode(handler);
+  effectNode.handler();
+  const cleanup = createCleanupEffect(effectNode);
+  return cleanup;
 }
